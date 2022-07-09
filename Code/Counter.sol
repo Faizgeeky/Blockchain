5b34389ad33f497975cda6eac24ae386360b9bce
Faizmohammad <42071980+Faizgeeky@users.noreply.github.com>
2022-07-09 21:30:46 +0530

    Create Counter.sol

---
+++ Code/Counter.sol
@@ -0,0 +1,21 @@
+// Day 3 - Solidity Tut 1
+pragma solidity ^0.6.0;
+
+contract Counter {
+    // Usigned Int also can't be negative..
+    uint count;
+
+    constructor() public{
+
+    }
+    
+    // read 
+    function getCount() public view returns(uint){
+        return count;
+    }
+
+    // write
+    function setCount() public {
+        count = count + 1;
+    }
+}
